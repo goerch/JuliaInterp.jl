c0f11d21dadaa2887736521610b6acc7ede0459c
goerch <jhr.walter@t-online.de>
2021-12-21 23:03:36 +0100

    Fixing benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,14 +5,14 @@ const SUITE = BenchmarkGroup()
 
 # Recursively call itself
 f(i, j) = i == 0 ? j : f(i - 1, j + 1)
-SUITE["recursive self 1_000"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("f(1_000, 0)"), 1000)
+SUITE["recursive self 1_000"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("f(1_000, 0)"), false, UInt64(1000))
 
 # Long stack trace calling other functions
 f0(i) = i
 for i in 1:1_000
     @eval $(Symbol("f", i))(i) = $(Symbol("f", i-1))(i)
 end
-SUITE["recursive other 1_000"] = @benchmarkable Base.@invokelatest JuliaInterp.interprete_ast(Main, Meta.parse("f1000(1)"), 1000)
+SUITE["recursive other 1_000"] = @benchmarkable Base.@invokelatest JuliaInterp.interpret_ast(Main, Meta.parse("f1000(1)"), false, UInt64(1000))
 
 # Tight loop
 function f(X)
@@ -23,7 +23,7 @@ function f(X)
     return s
 end
 const X = rand(1:10, 10_000)
-SUITE["tight loop 10_000"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("f(X)"), 1000) 
+SUITE["tight loop 10_000"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("f(X)"), false, UInt64(1000)) 
 
 # Throwing and catching an error over a large stacktrace
 function g0(i)
@@ -37,7 +37,7 @@ for i in 1:1_000
     @eval $(Symbol("g", i))(i) = $(Symbol("g", i+1))(i)
 end
 g1001(i) = error()
-SUITE["throw long 1_000"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("g0(1)"), 1000) 
+SUITE["throw long 1_000"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("g0(1)"), false, UInt64(1000)) 
 
 # Function with many statements
 macro do_thing(expr, N)
@@ -53,16 +53,16 @@ function counter()
     @do_thing(a = a + 1, 5_000)
     return a
 end
-SUITE["long function 5_000"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("counter()"), 1000) 
+SUITE["long function 5_000"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("counter()"), false, UInt64(1000)) 
 
 # Ccall
 function ccall_ptr(ptr, x, y)
     ccall(ptr, Int, (Int, Int), x, y)
 end
 const ptr = @cfunction(+, Int, (Int, Int))
-SUITE["ccall ptr"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("ccall_ptr(ptr, 1, 5)"), 1000) 
+SUITE["ccall ptr"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("ccall_ptr(ptr, 1, 5)"), false, UInt64(1000)) 
 
 function powf(a, b)
     ccall(("powf", Base.Math.libm), Float32, (Float32,Float32), a, b)
 end
-SUITE["ccall library"] = @benchmarkable JuliaInterp.interprete_ast(Main, Meta.parse("powf(2, 3)"), 1000) 
+SUITE["ccall library"] = @benchmarkable JuliaInterp.interpret_ast(Main, Meta.parse("powf(2, 3)"), false, UInt64(1000)) 
